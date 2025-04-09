d7a25c7650e7d267b583c89619afbb70ade0b689
Souradip <dgsouro77@gmail.com>
2025-04-10 00:48:41 +0530

    Clerk setup added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "@/styles/globals.css";
 
 import { type Metadata } from "next";
 import { Geist } from "next/font/google";
-
+import {ClerkProvider} from "@clerk/nextjs"
 import { TRPCReactProvider } from "@/trpc/react";
 
 export const metadata: Metadata = {
@@ -19,11 +19,15 @@ const geist = Geist({
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  
   return (
-    <html lang="en" className={`${geist.variable}`}>
+    <ClerkProvider>
+     <html lang="en" className={`${geist.variable}`}>
       <body>
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
+  </ClerkProvider>
+   
   );
 }
