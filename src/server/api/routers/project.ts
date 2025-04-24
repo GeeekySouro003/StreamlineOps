dc6dcb44e5628f82fc1e18e32a4df31a545c2065
Souradip <dgsouro77@gmail.com>
2025-04-24 22:12:26 +0530

    Added zod validation in project router

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -1,7 +1,16 @@
+import { z } from "zod";
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const projectRouter = createTRPCRouter({
-    createProject:protectedProcedure.input().mutation(async({ctx,input})=>{
+    createProject:protectedProcedure.input(
+        z.object({
+            name:z.string(),
+            githubUrl:z.string(),
+            githubToken:z.string().optional()
+        })
+
+        
+    ).mutation(async({ctx,input})=>{
         ctx.user.userId
         console.log('hi');
         return true;
